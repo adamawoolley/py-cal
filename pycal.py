bbd659fccd2837465b681648f04c6f3a3e686a94
adamawoolley <adamawoolley@gmail.com>
2018-05-19 08:23:51 -0700

    Updated to find the mean of a data set

--- pycal.py
+++ pycal.py
@@ -1,15 +1,45 @@
-import argparse
-
+input_error = 'Please enter a valid input'
+options = '''
+    There are currently 2 options
+    1. Input an equation and get the answer
+    2. Input mean followed by a list of numbers separated by spaces
+    '''
 def equation(equation):
+    if '^' in equation:
+        equation = str.replace(equation, '^', '**')
+
     try:
-        return eval(args.equation)
+        return eval(equation)
+
+    except:
+        return input_error
     
-    except NameError:
-        return 'Please type a valid input'
+def mean(numbers):
+    mean = 0
+
+    #removes white space from list
+    for i in numbers[1:]:
+        try:
+            float(i)
+        except:
+            numbers.remove(i)
+
+    for i in numbers[1:]:
+        try:
+            mean += float(i)
+        except:
+            return input_error
+    try:
+        return mean / (len(numbers) - 1)
+    except:
+        return input_error
     
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-e', '--equation', help = 'Returns the answer to an inputed equation')
-    args = parser.parse_args()
-    if args.equation:
-        print(equation(args.equation))
+    intro = '''
+        Welcome to the python calculator.
+        To use this simply type in the equation you want answered.
+        To get a list of other uses type help.
+        To exit type ctr-c or delete
+        '''
+
+    print(intro)
